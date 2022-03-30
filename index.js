c34460c03a343fa927d15b8d6ad2289adef16f73
karage <josephbkarage@idreamtz.com>
2022-03-30 14:56:02 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const numbers = [1, -3, -4, 2, 89, 9, 20, 'wewe']
+
+function sumOfPositives(array) {
+return array
+    .filter(n => n >= 0)
+    .reduce((accumulator, currentValue) => accumulator + currentValue)
+}
+
+sumOfPositives(numbers);
