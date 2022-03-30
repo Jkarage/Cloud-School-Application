2634adaa731c83ff24ddeb20c21dccbbefd05ced
node_modules <josephbkarage@gmail.com>
2022-03-30 20:52:12 +0000

    Initial commit

---
+++ docker/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+	const app = express();
+	const router = express.Router();
+	
+
+	const path = __dirname + "/views/";
+	const port = 5000;
+	
+
+	router.get("/", function (req, res) {
+			  res.sendFile(path + "index.html");
+			});
+	
+
+	app.use(express.static(path));
+	app.use("/", router);
+	
+
+	app.listen(port, function () {
+			  console.log("server listening on port 5000!");
+			});
