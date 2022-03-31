77d19b0d642b03ad5798197dbe50d1c42c07aa40
karage <josephbkarage@idreamtz.com>
2022-03-31 09:12:41 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, -3, -4, 2, 89, 9, 20, 34, 'wewe']
-
-function sumOfPositives(array) {
-return array
-    .filter(n => n >= 0)
-    .reduce((accumulator, currentValue) => accumulator + currentValue)
-}
-
-sumOfPositives(numbers);
